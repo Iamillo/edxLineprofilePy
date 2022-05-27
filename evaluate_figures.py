78719b8e5149ec544c3de153bb937107cecb7d58
Oliver Schmitt <Oliver.Schmitt@d-fine.com>
2022-05-27 14:17:55 +0200

    some cleanup and formatting

--- evaluate_figures.py
+++ evaluate_figures.py
@@ -9,12 +9,11 @@ from plotting import plotAttime
 
 
 def evaluate_print_figures(values: dict, eval_time: list, elements: list) -> None:
-    number_subplots = len(eval_time) * len(elements)
     fig, axs = mapy.subplots(len(eval_time),
-                            len(elements),
-                            sharex='col',
-                            sharey='col',
-                            gridspec_kw={'hspace':0, 'wspace':0.25})
+                             len(elements),
+                             sharex='col',
+                             sharey='col',
+                             gridspec_kw={'hspace':0, 'wspace':0.25})
     fig.suptitle("time", x=.94)
 
     for e in range(0, len(elements)):
@@ -29,23 +28,18 @@ def evaluate_print_figures(values: dict, eval_time: list, elements: list) -> Non
         for t in range(0, len(eval_time)):
             time = list(eval_time)[t]
             axs[t, e].set_xlim(0, 90)
-            axs[t, e].plot(values[time]['x'], values[time][elem], ':', color='lightgray')  # , marker, label=elem + time)
-            axs[t, e].plot(values[time]['x'], savgol_filter(values[time][elem]), color=colors[elem])#, marker, label=elem + time)
+            axs[t, e].plot(values[time]['x'], values[time][elem], ':', color='lightgray')
+            axs[t, e].plot(values[time]['x'], savgol_filter(values[time][elem]), color=colors[elem])
 
             if e == 2:
-                axs[t, e].text(100, 9, str(int(time.replace('sec',''))) + ' s', fontsize=20)
-    #        if e == 0:
-    #            axs[t, e].set_yticks([35, 50])
-    #        if e == 1:
-    #            axs[t, e].set_yticks([41, 45])
-    #        if e == 2:
-    #            axs[t, e].set_yticks([7, 12])
-            if e == 0 and t == round(len(eval_time) /2)-1:
+                axs[t, e].text(100, 9, str(int(time.replace('sec', ''))) + ' s', fontsize=20)
+            if e == 0 and t == round(len(eval_time) / 2) - 1:
                 axs[t, e].set_ylabel('c / at.%', fontsize=20)
             axs[t, e].tick_params(labelsize=15)
 
     fig.savefig(os.path.join(config.results_directory, '3E_col.png'), dpi=300)
     mapy.show()
+
 
 def delta(position, peaksOverTime=None, deviationOverTime=None):
     # plotting delta c = c_twin - c_matrix VS time
